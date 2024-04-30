f97306f63481b0672d8a122588e85e1d8a0c57a4
Eduardo <eduardolustosa26@gmail.com>
2024-04-30 17:59:24 -0300

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <main>
+      <h1>Amazon Searcher</h1>
+
+      <form>
+        <input type="text" placeholder="Search" />
+      </form>
+    </main>
+  )
+}
